4bf35444ad7637ffeb938dd001c32f5690b7e1a2
SD <shrish.deshpande@icloud.com>
2022-01-05 21:32:58 +0530

    Mod stuff

---
+++ src/main/java/io/github/boogiemonster1o1/lichessmc/LichessMC.java
@@ -0,0 +1,14 @@
+package io.github.boogiemonster1o1.lichessmc;
+
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LichessMC implements ModInitializer {
+	public static final Logger LOGGER = LogManager.getLogger("modid");
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/io/github/boogiemonster1o1/lichessmc/mixin/ExampleMixin.java
@@ -0,0 +1,9 @@
+package io.github.boogiemonster1o1.lichessmc.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import net.minecraft.client.gui.screen.TitleScreen;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+}
